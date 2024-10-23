6e55aef654d46e2d0a0e4f21cf46a161a2b94040
chicodespons <54290946+chicodespons@users.noreply.github.com>
2024-10-23 18:05:59 +0200

    webpacksvg config change

--- next.config.mjs
+++ next.config.mjs
@@ -20,10 +20,39 @@ const nextConfig = {
     ]
   },
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+
+    // Check if the fileLoaderRule exists before proceeding
+    if (fileLoaderRule) {
+      // Modify the file loader rule to ignore *.svg
+      fileLoaderRule.exclude = /\.svg$/i;
+
+      // Push the new rules for handling SVGs
+      config.module.rules.push(
+        // Reapply the existing rule, but only for svg imports ending in ?url
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // *.svg?url
+        },
+        // Convert all other *.svg imports to React components
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+          use: ['@svgr/webpack'],
+        }
+      );
+    } else {
+      // If there's no existing rule for SVGs, simply add the SVGR rule
+      config.module.rules.push({
+        test: /\.svg$/i,
+        use: ['@svgr/webpack'],
+      });
+    }
 
     return config;
   },
