11ede9d666ec2ae3a285338f3534c3a68354e2c1
chicodespons <54290946+chicodespons@users.noreply.github.com>
2024-11-09 20:07:42 +0100

    test revalidate 2

--- app/components/CardSection.tsx
+++ app/components/CardSection.tsx
@@ -6,20 +6,12 @@ type CardSectionProps = {
   locale: string
 }
 
-const shouldFetchData = false; // Set this to false to skip fetching
-
-
 async function fetchPijlers(locale: string) {
   
-  if (!shouldFetchData) {
-    return null; // Skip fetching and return null
-  }
-
   const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/pijlers?locale=${locale}`, {
     headers: {
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`,
-    },
-    cache: 'force-cache'
+    }
   });
   const data = await res.json();
   return data.data; 
