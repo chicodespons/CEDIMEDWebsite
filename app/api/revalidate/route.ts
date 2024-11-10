4567dc35b2873c063b4e259ff8bdfab7bc002808
chicodespons <54290946+chicodespons@users.noreply.github.com>
2024-11-10 20:50:15 +0100

    rebuild trigger2

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 
 const appId = '573d137f-a7ae-4150-a581-c7800106d480'; // Replace with your DigitalOcean app ID
-const digitalOceanToken = 'dop_v1_92c4db031651bedc323a0b8db8cc0d5d2703727dfe10adbaca6e4d91be2f43d2'; // Store your token in an environment variable for security
+const digitalOceanToken = 'dop_v1_8247326c19043fe449c6f38afa297570a57a00982cce6ac17de971a763fdaead'; // Store your token in an environment variable for security
 
 export async function POST(request: Request) {
   // Secure the webhook using a secret token
