c5e9c54aee59964d11112006b766b329af1ac2b3
chicodespons <54290946+chicodespons@users.noreply.github.com>
2024-11-14 20:20:36 +0100

    test locale for clinical-care

--- app/[locale]/clinical-care/page.tsx
+++ app/[locale]/clinical-care/page.tsx
@@ -2,11 +2,12 @@ import PijlerText from "../../components/PijlerText";
 import { setRequestLocale } from "next-intl/server";
 
 type Params = {
-  locale: string | string[];
+  params : {
+    locale: string
+  }
 };
 
-export default function ClinicalCare({ params} : {params: Params}) {
-  const locale = Array.isArray(params?.locale) ? params.locale[0] : params.locale || "nl"; // Default to 'nl'
+export default function ClinicalCare({ params: { locale }}: Params) {
   setRequestLocale(locale);
     return (
       <div>
