2710ef72045cf580fecce04784c17015f735faed
chicodespons <54290946+chicodespons@users.noreply.github.com>
2024-11-18 21:35:25 +0100

    strapi api url

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -41,7 +41,7 @@ export function flattenAttributes(data: any): any {
   }
 
 export function getStrapiURL() {
-    return process.env.NEXT_PUBLIC_STRAPI_URL ?? "http://localhost:1337";
+    return process.env.NEXT_PUBLIC_STRAPI_API_URL ?? "http://localhost:1337";
   }
   
 export function getStrapiMedia(url: string | null) {
