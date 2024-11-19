0ea8bc69924f74290b01b083e15989b59a0a4518
chicodespons <54290946+chicodespons@users.noreply.github.com>
2024-11-19 22:55:20 +0100

    ts-expec-error -instead

--- app/components/news/NieuwsSection.tsx
+++ app/components/news/NieuwsSection.tsx
@@ -22,8 +22,8 @@ async function fetchNieuwsItems(locale: string) : Promise<NieuwsItem[]> {
   });
 
   const data = await res.json();
-  
-  // @ts-ignore
+
+  // @ts-expect-error
   return data.data.map((item: any) => ({
     id: item.id,
     excerpt: item.excerpt,
