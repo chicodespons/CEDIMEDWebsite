349fb948cf7cf3c2b48f116a687222f8aa5ed2dd
chicodespons <54290946+chicodespons@users.noreply.github.com>
2024-12-05 22:06:14 +0100

    test locale

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -2,23 +2,21 @@ import { getTranslations, setRequestLocale } from "next-intl/server";
 import ContactForm from "../../components/ContactForm";
 import GoogleMapsComponent from "../../components/GoogleMapsComponent";
 
-type Params = {
-  locale: string | string[];
+type ContactPageProps = {
+  params : {
+    locale: string
+  }
 };
 
-export default async function Contact({
-  params,
-}: {
-  params: Params;
-}) {
-  const locale = Array.isArray(params?.locale) ? params.locale[0] : params.locale || "nl"; // Default to 'nl'
+
+export default async function Contact({ params: { locale }}: ContactPageProps) {
   setRequestLocale(locale);
-  const t = await getTranslations();
-  
-    return (
+  const t = await getTranslations()
+
+  return (
       <div>
         <GoogleMapsComponent apiKey={"AIzaSyAsiywReNSwy8qFOpnPeHAHnSN9TkHXy74"} t={t} />
         <ContactForm />
       </div>
-    );
-  }
+  );
+}
