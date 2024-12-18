f5f82523d453a3592bd873d61eadcc7ed84f96a7
chicodespons <54290946+chicodespons@users.noreply.github.com>
2024-12-18 11:02:42 +0100

    test 2 metadata

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -2,6 +2,10 @@ import { Metadata } from "next";
 import AboutText from "../../components/AboutText";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 
+type Params = {
+  locale: string | string[];
+};
+
 export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
   const locale = Array.isArray(params?.locale) ? params.locale[0] : params.locale || "nl"; // Default to 'nl'
   const t = await getTranslations(locale);
@@ -11,10 +15,6 @@ export async function generateMetadata({ params }: { params: Params }): Promise<
     description: t("overOnsDescription"),
   };
 }
-
-type Params = {
-  locale: string | string[];
-};
 
 
 export default async function About({
