ba0d4fef518bb20b30190774a8c74c934389d1e5
chicodespons <54290946+chicodespons@users.noreply.github.com>
2024-12-18 11:14:23 +0100

    test 3 metadata

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,21 +1,36 @@
 import { Metadata } from "next";
 import AboutText from "../../components/AboutText";
-import { getTranslations, setRequestLocale } from "next-intl/server";
+import {setRequestLocale } from "next-intl/server";
 
 type Params = {
   locale: string | string[];
 };
 
+const englishMetadata = {
+  title: "CEDIMED Brussels",
+  description: "Over the organization CEDIMED Brussels",
+};
+const frenchMetadata = {
+  title: "CEDIMED Bruxelles",
+  description: "Sur l'organisation CEDIMED Bruxelles",
+};
+const dutchMetadata = {
+  title: "CEDIMED Brussels",
+  description: "Over de organisatie CEDIMED Brussels",
+};
+
 export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
-  const locale = Array.isArray(params?.locale) ? params.locale[0] : params.locale || "nl"; // Default to 'nl'
-  const t = await getTranslations(locale);
-
-  return {
-    title: t("overOns"), 
-    description: t("overOnsDescription"),
-  };
+  const locale =  Array.isArray(params?.locale) ? params.locale[0] : params.locale || "nl";
+  switch (locale) {
+    case "en":
+      return englishMetadata;
+    case "fr":
+      return frenchMetadata;
+    case "nl":
+    default:
+      return dutchMetadata;
+  }
 }
-
 
 export default async function About({
   params,
