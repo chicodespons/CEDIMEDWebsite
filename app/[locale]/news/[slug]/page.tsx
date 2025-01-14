4e1183b610fc1025cf299cf51d4993c122350e73
chicodespons <54290946+chicodespons@users.noreply.github.com>
2025-01-14 22:08:15 +0100

    test with delete

--- app/[locale]/news/[slug]/page.tsx
+++ app/[locale]/news/[slug]/page.tsx
@@ -3,149 +3,174 @@ import NoNewsComponent from "@/app/components/news/NoNewsComponent";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 
 type Props = {
-    params: {
-      slug?: string;
-      locale: string | string[]
+  params: {
+    slug?: string;
+    locale: string | string[];
+  };
+};
+
+interface StrapiImage {
+  alternativeText: string;
+  formats: {
+    medium: {
+      url: string;
+      width: number;
+      height: number;
+    };
+    thumbnail: {
+      url: string;
+      width: number;
+      height: number;
     };
   };
-
-  interface StrapiImage {
-    alternativeText: string;
-    formats: {
-      medium : {
-        url: string;
-        width: number;
-        height: number;
-      }
-      thumbnail: {
-        url: string;
-        width: number;
-        height: number;
-      }
-    }
-  }
-
-  interface ApiAuthor {
-    name: string;
-    bio: string;
-    avatar?: StrapiImage;
-  }
-  
-  interface ApiNewsItem {
-    id: number;
-    title: string;
-    content: Array<any>;
-    excerpt: string;
-    slug: string;
-    author?: ApiAuthor;
-    publishedAt: string;
-    image?: StrapiImage;
-  }
-  
-  interface ApiResponse {
-    data: ApiNewsItem;
-  }
-
-  interface NieuwsItem {
-    id: number;
-    title: string;
-    content: Array<any>;
-    excerpt: string;
-    slug: string;
-    publicationDate: string;
-    img: StrapiImage | null; 
-    author: string | null
-    bio: string | null;
-    avatar: StrapiImage | null;
 }
 
-  async function fetchNewsItemViaSlug(slug: string, locale: string) : Promise<NieuwsItem | null> {
-    try {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/newses?filters[slug][$eq]=${slug}&populate[author][populate]=avatar&populate=image&locale=${locale}`, 
+interface ApiAuthor {
+  name: string;
+  bio: string;
+  avatar?: StrapiImage;
+}
+
+interface ApiNewsItem {
+  id: number;
+  title: string;
+  content: Array<any>;
+  excerpt: string;
+  slug: string;
+  author?: ApiAuthor;
+  publishedAt: string;
+  image?: StrapiImage;
+}
+
+interface ApiResponse {
+  data: ApiNewsItem;
+}
+
+interface NieuwsItem {
+  id: number;
+  title: string;
+  content: Array<any>;
+  excerpt: string;
+  slug: string;
+  publicationDate: string;
+  img: StrapiImage | null;
+  author: string | null;
+  bio: string | null;
+  avatar: StrapiImage | null;
+}
+
+async function fetchNewsItemViaSlug(
+  slug: string,
+  locale: string
+): Promise<NieuwsItem | null> {
+  try {
+    let res = await fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/newses?filters[slug][$eq]=${slug}&populate[author][populate]=avatar&populate=image&locale=${locale}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`,
+        },
+      }
+    );
+
+    let data: ApiResponse = await res.json();
+
+    if (
+      (!data.data || !Array.isArray(data.data) || data.data.length === 0) &&
+      locale !== "nl"
+    ) {
+      // Fallback to the default locale (e.g., "nl")
+      res = await fetch(
+        `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/newses?filters[slug][$eq]=${slug}&populate[author][populate]=avatar&populate=image&locale=nl`,
         {
           headers: {
             Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`,
           },
         }
       );
-    
-      const data: ApiResponse = await res.json();
-      if (data.data && Array.isArray(data.data) && data.data.length > 0) {
-        const newsItem: ApiNewsItem = data.data[0];
-
-        const mappedItem: NieuwsItem = {
-          id: newsItem.id,
-          title: newsItem.title,
-          content: newsItem.content,
-          excerpt: newsItem.excerpt,
-          slug: newsItem.slug,
-          publicationDate: newsItem.publishedAt,
-          img: newsItem.image ?? null, 
-          author: newsItem.author ? newsItem.author.name : null, 
-          bio: newsItem.author ? newsItem.author.bio : null,   
-          avatar: newsItem.author && newsItem.author.avatar ? newsItem.author.avatar : null, 
-        };
-  
-        return mappedItem;
-      }
-  
-      return null; // Return null if no valid news item is found
-    
-    } catch (error) {
-      console.error("Error fetching news:", error);
+      data = await res.json();
     }
-    return null;
+
+    if (data.data && Array.isArray(data.data) && data.data.length > 0) {
+      const newsItem: ApiNewsItem = data.data[0];
+
+      const mappedItem: NieuwsItem = {
+        id: newsItem.id,
+        title: newsItem.title,
+        content: newsItem.content,
+        excerpt: newsItem.excerpt,
+        slug: newsItem.slug,
+        publicationDate: newsItem.publishedAt,
+        img: newsItem.image ?? null,
+        author: newsItem.author ? newsItem.author.name : null,
+        bio: newsItem.author ? newsItem.author.bio : null,
+        avatar:
+          newsItem.author && newsItem.author.avatar
+            ? newsItem.author.avatar
+            : null,
+      };
+
+      return mappedItem;
+    }
+
+    return null; // Return null if no valid news item is found
+  } catch (error) {
+    console.error("Error fetching news:", error);
   }
-  
- 
-  export async function generateMetadata({ params } : Props) {
-    const locale = Array.isArray(params?.locale) ? params.locale[0] : params.locale || "nl"; // Default to 'nl'
-    setRequestLocale(locale);
-    let newsItem : NieuwsItem = null;
-    let imgUrl : string = `${process.env.NEXT_PUBLIC_BASE_URL}/images/opengraph-image.png`
-    if (params.slug) {
+  return null;
+}
+
+export async function generateMetadata({ params }: Props) {
+  const locale = Array.isArray(params?.locale)
+    ? params.locale[0]
+    : params.locale || "nl"; // Default to 'nl'
+  setRequestLocale(locale);
+  let newsItem: NieuwsItem | null = null;
+
+  if (params.slug) {
+    try {
       newsItem = await fetchNewsItemViaSlug(params.slug, locale);
-     }
-    
-    if (newsItem?.img?.formats?.medium?.url) {
-      imgUrl = `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${newsItem.img.formats.medium.url}`
-    }
-    
-    return {
-      title: newsItem.title || "CEDIMED Brussels News",
-      description: newsItem.excerpt || "CEDIMED Brussels latest news",
-      openGraph: {
-        images : [
-          {
-            url: imgUrl
-          }
-        ]
-      }
+    } catch (error) {
+      console.error("Error in generateMetadata fetchNewsItemViaSlug:", error);
     }
   }
-  
 
-  export default async function NewsPage({ params } : Props ) {
-    const locale = Array.isArray(params?.locale) ? params.locale[0] : params.locale || "nl"; // Default to 'nl'
-    setRequestLocale(locale);
-    let newsItem : NieuwsItem = null;
-    const t = await getTranslations();
+  const imgUrl = newsItem?.img?.formats?.medium?.url
+    ? `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${newsItem.img.formats.medium.url}`
+    : `${process.env.NEXT_PUBLIC_BASE_URL}/images/opengraph-image.png`;
 
-   if (params.slug) {
+  return {
+    title: newsItem?.title || "CEDIMED Brussels News",
+    description: newsItem?.excerpt || "CEDIMED Brussels latest news",
+    openGraph: {
+      images: [
+        {
+          url: imgUrl,
+        },
+      ],
+    },
+  };
+}
+
+export default async function NewsPage({ params }: Props) {
+  const locale = Array.isArray(params?.locale)
+    ? params.locale[0]
+    : params.locale || "nl"; // Default to 'nl'
+  setRequestLocale(locale);
+  let newsItem: NieuwsItem = null;
+  const t = await getTranslations();
+
+  if (params.slug) {
     newsItem = await fetchNewsItemViaSlug(params.slug, locale);
-   }
-
-   if (newsItem != null) {
-    return (
-      <NewsComponent newsItem={newsItem} />  
-  )
-   } else {
-    return <NoNewsComponent t={t} />
-   }
-
-    
   }
 
+  if (!newsItem && params.slug) {
+    newsItem = await fetchNewsItemViaSlug(params.slug, "nl");
+  }
 
-
+  if (newsItem) {
+    return <NewsComponent newsItem={newsItem} />;
+  } else {
+    return <NoNewsComponent t={t} />;
+  }
+}
