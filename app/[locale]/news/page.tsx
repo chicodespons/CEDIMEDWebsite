3dee1d25e2f1ce1f2bfa0e6273b4819413627c69
chicodespons <54290946+chicodespons@users.noreply.github.com>
2025-01-14 22:56:25 +0100

    changes in news page

--- app/[locale]/news/page.tsx
+++ app/[locale]/news/page.tsx
@@ -52,9 +52,9 @@ interface NieuwsItem {
     avatar: StrapiImage | null;
 }
 
-async function fetchLatestNewsItem(locale:string): Promise<NieuwsItem| null> {
+async function fetchLatestNewsItem(locale: string): Promise<NieuwsItem | null> {
     try {
-      let res = await fetch(
+      const res = await fetch(
         `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/newses?sort[0]=publishedAt:desc&populate[author][populate]=avatar&populate=image&locale=${locale}`,
         {
           headers: {
@@ -63,72 +63,61 @@ async function fetchLatestNewsItem(locale:string): Promise<NieuwsItem| null> {
         }
       );
   
-      let data: ApiResponse = await res.json();
-
-      if(!data.data || !Array.isArray(data.data) || data.data.length === 0) {
-        //retry with nl as fallback
-
-        res =  await fetch(
-          `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/newses?sort[0]=publishedAt:desc&populate[author][populate]=avatar&populate=image&locale=nl`,
-          {
-            headers: {
-              Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`,
-            },
-          }
-        );
-  
-        data = await res.json();
-      }
-
-      if (data.data && Array.isArray(data.data) && data.data.length > 0) {
-        const newsItem: ApiNewsItem = data.data[0];
-  
-        const mappedItem: NieuwsItem = {
-          id: newsItem.id,
-          title: newsItem.title,
-          content: newsItem.content,
-          excerpt: newsItem.excerpt,
-          slug: newsItem.slug,
-          publicationDate: newsItem.publishedAt,
-          img: newsItem.image ?? null,
-          author: newsItem.author ? newsItem.author.name : null,
-          bio: newsItem.author ? newsItem.author.bio : null,
-          avatar: newsItem.author && newsItem.author.avatar ? newsItem.author.avatar : null,
-        };
-  
-        return mappedItem;
+      if (!res.ok) {
+        console.error(`Failed to fetch news for locale ${locale}. Status: ${res.status}`);
+        return null;
       }
   
-      return null; // Return null if no valid news item is found
+      const data: ApiResponse = await res.json();
+  
+      if (!data?.data || !Array.isArray(data.data) || data.data.length === 0) {
+        console.warn(`No news found for locale ${locale}`);
+        return null;
+      }
+  
+      const newsItem = data.data[0];
+      return {
+        id: newsItem.id,
+        title: newsItem.title,
+        content: newsItem.content,
+        excerpt: newsItem.excerpt,
+        slug: newsItem.slug,
+        publicationDate: newsItem.publishedAt,
+        img: newsItem.image ?? null,
+        author: newsItem.author?.name || null,
+        bio: newsItem.author?.bio || null,
+        avatar: newsItem.author?.avatar || null,
+      };
     } catch (error) {
       console.error("Error fetching latest news:", error);
+      return null;
     }
-    return null;
-    }
+  }
   
-// export async function generateMetadata({ params }: { params: { locale: string } }) {
-//   const locale = Array.isArray(params?.locale) ? params.locale[0] : params.locale || "nl";
-//   setRequestLocale(locale);
+  
+export async function generateMetadata({ params }: { params: { locale: string } }) {
+  const locale = Array.isArray(params?.locale) ? params.locale[0] : params.locale || "nl";
+  setRequestLocale(locale);
 
-//   const newsItem: NieuwsItem | null = await fetchLatestNewsItem(locale);
-//   let imgUrl: string = `${process.env.NEXT_PUBLIC_BASE_URL}/images/opengraph-image.png`;
+  const newsItem: NieuwsItem | null = await fetchLatestNewsItem(locale);
+  let imgUrl: string = `${process.env.NEXT_PUBLIC_BASE_URL}/images/opengraph-image.png`;
 
-//   if (newsItem?.img?.formats?.medium?.url) {
-//     imgUrl = `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${newsItem.img.formats.medium.url}`
-//   }
+  if (newsItem?.img?.formats?.medium?.url) {
+    imgUrl = `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${newsItem.img.formats.medium.url}`
+  }
 
-//   return {
-//     title: newsItem?.title || "CEDIMED Brussels News",
-//     description: newsItem?.excerpt || "CEDIMED Brussels latest news",
-//     openGraph: {
-//       images: [
-//         {
-//           url: imgUrl
-//         }
-//       ]
-//     }
-//   }
-// }
+  return {
+    title: newsItem?.title || "CEDIMED Brussels News",
+    description: newsItem?.excerpt || "CEDIMED Brussels latest news",
+    openGraph: {
+      images: [
+        {
+          url: imgUrl
+        }
+      ]
+    }
+  }
+}
 
 export default async function NewsBasePage({ params }: { params: { locale: string } }) {
     const locale = Array.isArray(params?.locale) ? params.locale[0] : params.locale || "nl";
