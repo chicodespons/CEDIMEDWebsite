a47716b254db584cf434038d352eb4420cd4b19a
chicodespons <54290946+chicodespons@users.noreply.github.com>
2025-01-16 21:41:17 +0100

    layout text uniformized

--- app/components/PijlerText.tsx
+++ app/components/PijlerText.tsx
@@ -1,53 +1,57 @@
-import React from 'react'
-import RichTextRenderer from './RichTextRenderer';
+import React from "react";
+import RichTextRenderer from "./RichTextRenderer";
 
-
-async function fetchPijler(locale: string, slug:string) {
-
+async function fetchPijler(locale: string, slug: string) {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/pijlers?filters[link][$eq]=${slug}&locale=${locale}`, {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`,
-      },
-    });
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/pijlers?filters[link][$eq]=${slug}&locale=${locale}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`,
+        },
+      }
+    );
 
     const data = await res.json();
-    console.log(JSON.stringify(data,null,2))
+    console.log(JSON.stringify(data, null, 2));
     return data.data?.[0] || null;
-
-  } catch (error){
-    console.log("data not fetched correctly: " ,error)
+  } catch (error) {
+    console.log("data not fetched correctly: ", error);
   }
-    return null;    
+  return null;
+}
+
+const PijlerText = async ({
+  locale,
+  slug,
+}: {
+  locale: string;
+  slug: string;
+}) => {
+  const pijler = await fetchPijler(locale, slug);
+
+  console.dir(pijler, { depth: null });
+
+  if (!pijler) {
+    return <p>No data found for the provided locale and slug.</p>;
   }
 
-const PijlerText = async ({locale,slug}: {locale: string, slug:string }) => {
-    const pijler = await fetchPijler(locale, slug);
+  return (
+    <section className="py-14 bg-white mt-2">
+      <div className="container mx-auto">
+        <div className="bg-gray-100 p-4 mx-4 lg:mx-32 rounded text-justify">
+          {/* Center the h2 */}
+          <div className="text-center py-4">
+            <h2 className="text-2xl lg:text-3xl text-left lg:text-center font-bold px-4 py-2 uppercase tracking-wider">
+              {pijler.TextTitle}
+            </h2>
+          </div>
 
-    console.dir(pijler, { depth: null });
-
-
-    if (!pijler) {
-      return <p>No data found for the provided locale and slug.</p>;
-    }
-  
-    return (
-      <section className="py-14 bg-white mt-2">
-  <div className="container mx-auto px-6 text-justify">
-    <div className="max-w-[1200px] mx-auto bg-gray-100 p-4 rounded">
-      {/* Center the h2 */}
-      <div className="text-center py-4">
-        <h2 className="text-3xl font-bold text-center px-4 py-2 uppercase tracking-wider">
-        {pijler.TextTitle}
-        </h2>
+          <RichTextRenderer content={pijler.text} />
+        </div>
       </div>
+    </section>
+  );
+};
 
-      <RichTextRenderer content={pijler.text}/>
-    </div>
-  </div>
-</section>
-    );
-  };
-    
-
-export default PijlerText
+export default PijlerText;
