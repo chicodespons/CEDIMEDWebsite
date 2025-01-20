85a0d5aac73157e59dc312e13830b3519e4b4a4c
chicodespons <54290946+chicodespons@users.noreply.github.com>
2025-01-20 20:59:22 +0100

    a bit more margin for text news large

--- app/components/news/NewsComponent.tsx
+++ app/components/news/NewsComponent.tsx
@@ -106,37 +106,38 @@ export const NewsComponent: React.FC<NewsComponentProps> = async ({
   );
 
   return (
-    
     <div className="flex flex-col lg:flex-row gap-8 mx-auto items-start overflow-x-hidden">
       {/* MAIN CONTENT */}
       <main className="flex-1">
         <section className="py-14 bg-white mt-2">
           <div className="container mx-auto">
-          <div className=" bg-gray-100 p-4 rounded text-justify">
-            {/* This is the 'AboutText'-like wrapper */}
-            <div className="text-center py-4">
-              <h1 className="text-2xl lg:text-3xl text-left lg:text-center font-bold px-4 py-2 uppercase tracking-wider">
-                {title}
-              </h1>
-              <p className="text-gray-600 mb-6 text-left px-4 lg:text-center">
-                {`Published on ${formattedDate} by ${author}`}
-              </p>
-            </div>
-
-            {/* Image */}
-            {img && (
-              <div className="flex justify-center mb-4">
-                <img
-                  src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${img.formats.medium.url}`}
-                  alt={img.alternativeText || "News Item Image"}
-                  className="rounded max-w-full h-auto"
-                />
+            <div className=" bg-gray-100 p-4 rounded text-justify">
+              {/* This is the 'AboutText'-like wrapper */}
+              <div className="text-center py-4">
+                <h1 className="text-2xl lg:text-3xl text-left lg:text-center font-bold px-4 py-2 uppercase tracking-wider">
+                  {title}
+                </h1>
+                <p className="text-gray-600 mb-6 text-left px-4 lg:text-center">
+                  {`Published on ${formattedDate} by ${author}`}
+                </p>
               </div>
-            )}
 
-            {/* Rich text content */}
-            <RichTextRenderer content={content} />
-          </div>
+              {/* Image */}
+              {img && (
+                <div className="flex justify-center mb-4">
+                  <img
+                    src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${img.formats.medium.url}`}
+                    alt={img.alternativeText || "News Item Image"}
+                    className="rounded max-w-full h-auto"
+                  />
+                </div>
+              )}
+
+              {/* Rich text content */}
+              <div className="lg:mx-16">
+                <RichTextRenderer content={content} />
+              </div>
+            </div>
           </div>
         </section>
       </main>
@@ -159,9 +160,7 @@ export const NewsComponent: React.FC<NewsComponentProps> = async ({
             />
             <div>
               <h3 className="text-base font-semibold my-6">{author}</h3>
-              <p className="text-sm text-gray-600 leading-relaxed">
-                {bio}
-              </p>
+              <p className="text-sm text-gray-600 leading-relaxed">{bio}</p>
             </div>
           </div>
         </section>
