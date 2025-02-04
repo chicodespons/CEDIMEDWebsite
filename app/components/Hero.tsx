0d82e6baf91bddfe966b9c5424de842e035bbd55
chicodespons <54290946+chicodespons@users.noreply.github.com>
2025-02-04 22:57:47 +0100

    hero changes

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,13 +6,13 @@ const Hero = ({ t, locale }: { t: (key: string) => string, locale:string }) => {
   return (
     <div className="relative bg-grayBack h-[300px] sm:h-[500px] overflow-hidden mt-2">
       {/* Background Image with Border */}
-      <div className="relative h-full w-full border-grayBack border-[16px] sm:border-[24px]"> {/* Responsive border */}
+      <div className="relative h-full w-full border-grayBack border-[16px] sm:border-[24px]">
         <Image
-          src="/images/LuchtfotoUZB.webp" // Ensure this matches the correct image path
+          src="/images/LuchtfotoUZB.webp"
           alt="Emergency and Disaster Medicine"
           fill
-          className='object-cover object-center rounded-lg' // Use inline style for object fit and position
-          quality={75} // Optional: Set image quality
+          className='object-cover object-center rounded-lg'
+          quality={75}
           priority
         />
       </div>
@@ -24,8 +24,9 @@ const Hero = ({ t, locale }: { t: (key: string) => string, locale:string }) => {
       <div className="absolute inset-0 z-30 flex flex-col justify-center items-center text-center px-4">
         {/* Title */}
         <h1 className="text-xl uppercase tracking-wider md:text-3xl lg:text-4xl font-bold text-white mt-4 px-4 sm:mt-0 sm:px-0">
-        Centre on Emergency & Disaster Medicine Brussels       
+          Centre on Emergency & Disaster Medicine Brussels       
         </h1>
+        
         {/* Subtitle */}
         <p className="mt-4 text-xs md:text-xl text-white">
           {t("cedimedSlogan1")}
@@ -34,16 +35,16 @@ const Hero = ({ t, locale }: { t: (key: string) => string, locale:string }) => {
         </p>
 
         {/* Buttons */}
-        <div className="mt-8 mb-4 flex justify-center ">
+        <div className="mt-8 mb-6 flex flex-col sm:flex-row gap-4 sm:gap-6 justify-center">
           <Link
             href={`/${locale}/about`}
-            className="px-2 uppercase sm:px-8 py-2 sm:py-3 bg-uzGreen text-white font-bold rounded hover:bg-uzGray transition duration-300 text-sm sm:text-base"
+            className="px-6 sm:px-8 py-2 sm:py-3 bg-uzGreen text-white font-bold rounded hover:bg-uzGray transition duration-300 text-sm sm:text-base text-center"
           >
             {t("overOns")}
           </Link>
           <Link
             href={`/${locale}/contact`}
-            className="hidden sm:block px-2 uppercase sm:px-8 py-2 sm:py-3 bg-uzGreen text-white font-bold rounded hover:bg-uzGray transition duration-300 text-sm sm:text-base"
+            className="hidden sm:inline-block px-6 sm:px-8 py-2 sm:py-3 bg-uzGreen text-white font-bold rounded hover:bg-uzGray transition duration-300 text-sm sm:text-base text-center"
           >
             {t("contact")}
           </Link>
